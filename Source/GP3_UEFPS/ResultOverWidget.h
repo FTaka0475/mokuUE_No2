3cab714d4535bf5f85d198d2b23e74cea2e33e6b
FTaka <>
2026-01-15 16:30:34 +0900

    Lay作り直し

---
+++ Source/GP3_UEFPS/ResultOverWidget.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "ResultOverWidget.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class GP3_UEFPS_API UResultOverWidget : public UUserWidget
+{
+	GENERATED_BODY()
+	
+};
---
+++ Source/GP3_UEFPS/WBP_ResultOverLay.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "WBP_ResultOverLay.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class GP3_UEFPS_API UWBP_ResultOverLay : public UUserWidget
+{
+	GENERATED_BODY()
+	
+};
